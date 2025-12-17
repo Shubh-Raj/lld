6ed3f616d19633ad10172a4141173ee0f54f9349
Shubh Raj <shubhraj625@gmail.com>
2025-12-17 19:26:27 +0530

    Banking System and request

---
+++ PaymentGateway.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class PaymentRequest{
+    public:
+    string sender, receiver, currency;
+    double amount;
+    void setPaymentRequest(string& sender, string& receiver, string& currency, double amount){
+        this->sender = sender;
+        this->receiver = receiver;
+        this->currency = currency;
+        this->amount = amount;
+    }
+};
+//
+class BankingSystem{
+    public:
+    virtual bool processPayment(double amount) = 0;
+    virtual ~BankingSystem() {}
+};
+
+class PaytmBankingSystem : public BankingSystem {
+public:
+    PaytmBankingSystem() {}
+    bool processPayment(double amount) override {
+            int r = rand() % 100;
+            return r < 80;
+    }
+};
+
+class RazorpayBankingSystem : public BankingSystem {
+public:
+    RazorpayBankingSystem() {}
+    bool processPayment(double amount) override {
+        cout << "[BankingSystem-Razorpay] Processing payment of " << amount << "...\n";
+        int r = rand() % 100;
+        return r < 90;
+    }
+};
+
+class PaymentGateway{
+    protected:
+    BankingSystem* bankingSystem;
+    public:
+    virtual bool processPayment(PaymentRequest* request) {
+        if (!validatePayment(request)) {
+            cout << "[PaymentGateway] Validation failed for " << request->sender << ".\n";
+            return false;
+        }
+        if (!initiatePayment(request)) {
+            cout << "[PaymentGateway] Initiation failed for " << request->sender << ".\n";
+            return false;
+        }
+        if (!confirmPayment(request)) {
+            cout << "[PaymentGateway] Confirmation failed for " << request->sender << ".\n";
+            return false;
+        }
+        return true;
+    }
+
+    virtual bool validatePayment(PaymentRequest* request) = 0;
+    virtual bool initiatePayment(PaymentRequest* request) = 0;
+    virtual bool confirmPayment(PaymentRequest* request) = 0;
+};
+
+class PaytmGateway : public PaymentGateway {
+public:
+    PaytmGateway() {
+        bankingSystem = new PaytmBankingSystem();
+    }
+    bool validatePayment(PaymentRequest* request) override {
+        cout << "[Paytm] Validating payment for " << request->sender << ".\n";
+
+        if (request->amount <= 0 || request->currency != "INR") {
+            return false;
+        }
+        return true;
+    }
+    bool initiatePayment(PaymentRequest* request) override {
+        cout << "[Paytm] Initiating payment of " << request->amount 
+                  << " " << request->currency << " for " << request->sender << ".\n";
+
+        return bankingSystem->processPayment(request->amount);
+    }
+    bool confirmPayment(PaymentRequest* request) override {
+        cout << "[Paytm] Confirming payment for " << request->sender << ".\n";
+
+        return true;
+    }
+};
+
+class RazorpayGateway : public PaymentGateway {
+public:
+    RazorpayGateway() {
+        bankingSystem = new RazorpayBankingSystem();
+    }
+    bool validatePayment(PaymentRequest* request) override {
+        cout << "[Razorpay] Validating payment for " << request->sender << ".\n";
+
+        if (request->amount <= 0) {
+            return false;
+        }
+        return true;
+    }
+    bool initiatePayment(PaymentRequest* request) override {
+        cout << "[Razorpay] Initiating payment of " << request->amount 
+                  << " " << request->currency << " for " << request->sender << ".\n";
+
+        return bankingSystem->processPayment(request->amount);
+       
+    }
+    bool confirmPayment(PaymentRequest* request) override {
+        cout << "[Razorpay] Confirming payment for " << request->sender << ".\n";
+
+        return true;
+    }
+};
