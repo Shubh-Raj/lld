9e793ccaea995ee7622ab9de10f74e258856ee8d
Shubh Raj <shubhraj625@gmail.com>
2025-12-24 11:56:59 +0530

    car manager

--- CarRentalSystem/CarRentalSystem.cpp
+++ CarRentalSystem/CarRentalSystem.cpp
@@ -19,4 +19,51 @@ class Car{
     int getRentalPricePerDay(){
         return rentalPricePerDay;
     }
+    void getCarDetails(){
+        cout << "Make: " << make << endl;
+        cout << "Model: " << model << endl;
+        cout << "Type: " << type << endl;
+        cout << "License Plate No: " << licensePlateNo << endl;
+        cout << "Year: " << year << endl;
+        cout << "Rental Price Per Day: " << rentalPricePerDay << endl;
+    }
+};
+
+int Car::carId = 0;
+
+class SearchStrategy;
+
+class CarManager{
+    private:
+    static CarManager* instance;
+    vector<Car*> listOfCars;
+    SearchStrategy* searchStrategy;
+    CarManager(){}
+    public:
+    CarManager* getInstance(){
+        if(instance==nullptr){
+            instance = new CarManager();
+        }
+        return instance;
+    }
+    void setSearchStrategy(SearchStrategy* strategy){
+        searchStrategy = strategy;
+    }
+    void addCar(Car* car){
+        listOfCars.push_back(car);
+    }
+    void removeCar(Car* car){
+        auto it = find(listOfCars.begin(), listOfCars.end(), car);
+        if (it != listOfCars.end()) {
+            listOfCars.erase(it);
+        }
+    }
+    void getAllCarList(){
+        for(Car* car: listOfCars){
+            car->getCarDetails();
+        }
+    }
+    // vector<Car*> performSearch(){
+    //     return searchStrategy->findCars(listOfCars);
+    // }
 };
